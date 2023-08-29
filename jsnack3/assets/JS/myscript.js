c3771c9150e32f6ec56ff7e6d6a1300713b91ac1
Davide <davidemorandini98@gmail.com>
2023-08-29 16:00:09 +0200

    snack3 development

---
+++ jsnack3/assets/JS/myscript.js
@@ -0,0 +1,27 @@
+/* 
+    -   Il software deve chiedere per 10 volte all’utente di inserire un numero
+
+    -   Il programma stampa la somma di tutti i numeri inseriti
+*/
+
+// Raccoglimento
+
+let finalSum;
+
+// Elaborazione
+for (let i = 0; i < 10 ; i++) {
+
+    let askNumber = Number(prompt("Inserisci un numero a piacere"));
+    console.log(askNumber);
+
+    
+
+}
+
+
+
+
+
+
+// Restituzione 
+// document.getElementById("my_sum").innerHTML = finalSum;
