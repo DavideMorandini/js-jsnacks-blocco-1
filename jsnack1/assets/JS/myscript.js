08c3cd1384814edff17822a7d2d7543e95a726ef
Davide <davidemorandini98@gmail.com>
2023-08-30 10:55:08 +0200

    snack1 bonus

--- jsnack1/assets/JS/myscript.js
+++ jsnack1/assets/JS/myscript.js
@@ -4,24 +4,46 @@
     -   Il software stampa il maggiore
 */
 
-// Raccoglimento
-let numberChoice_1 = Number(prompt("Inserisci un numero a piacere"));
+// // Raccoglimento
+// let numberChoice_1 = Number(prompt("Inserisci un numero a piacere"));
 
-let numberChoice_2 = Number(prompt("Inserisci un numero a piacere"));
+// let numberChoice_2 = Number(prompt("Inserisci un numero a piacere"));
 
-let finalChoice;
+// let finalChoice;
 
-// Elaborazione
-if (numberChoice_1 > numberChoice_2) {
-    finalChoice = numberChoice_1;
-    console.log(numberChoice_1); 
+// // Elaborazione
+// if (numberChoice_1 > numberChoice_2) {
+//     finalChoice = numberChoice_1;
+//     console.log(numberChoice_1); 
 
-} else {
-    finalChoice = numberChoice_2;
-    console.log(numberChoice_2);
+// } else {
+//     finalChoice = numberChoice_2;
+//     console.log(numberChoice_2);
+// }
+
+// // Restituzione
+// document.getElementById("my_number").innerHTML = ("Il numero maggiore è" + " " + finalChoice);
+
+
+
+
+
+// Stesso ragionamento ma con il ciclo While
+
+let finalChoice = true;
+
+while (finalChoice) {
+    let numberChoice_1 = Number(prompt("Inserisci un numero a piacere"));
+
+    let numberChoice_2 = Number(prompt("Inserisci un numero a piacere"));
+
+    if (numberChoice_1 > numberChoice_2) {
+            finalChoice = numberChoice_1;
+            console.log(numberChoice_1); 
+        
+        } else {
+            finalChoice = numberChoice_2;
+            console.log(numberChoice_2);
+        }
 }
-
-// Restituzione
 document.getElementById("my_number").innerHTML = ("Il numero maggiore è" + " " + finalChoice);
-
-
